542c544139a64b3093199acdb55429b671a8ebd1
Enzo <duenzo1010@gmail.com>
2022-04-23 15:30:03 +0800

    feat: rtk-query IG stories

--- src/pages/home/components/IGStory/index.tsx
+++ src/pages/home/components/IGStory/index.tsx
@@ -1,17 +1,22 @@
 import Item from './Item';
-
-// TODO del
-import db from '../../../../db.json';
+import { useGetIGStoiesQuery } from 'services/homeServices';
+import Loading from 'components/Loading';
 
 const IGStory: React.FC = () => {
-  const data = db.stories;
+  const { data, isLoading, error } = useGetIGStoiesQuery('all');
+
   return (
     <div className='w-full h-[110px] box-border flex items-center overflow-x-auto overflow-y-hidden shadow-md no-scrollbar lg:my-8'>
-      {/* <Item name='enzo19901010' avatar='/images/avatars/a1.png' /> */}
-      {data?.map((item) => {
-        const { id, name, avatar } = item;
-        return <Item key={id} name={name} avatar={avatar} />;
-      })}
+      {isLoading && (
+        <div className='flex justify-center w-full'>
+          <Loading />
+        </div>
+      )}
+      {!isLoading &&
+        data?.map((item) => {
+          const { id, name, avatar } = item;
+          return <Item key={id} name={name} avatar={avatar} />;
+        })}
     </div>
   );
 };
